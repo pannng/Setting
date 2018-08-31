f591a41a4a7a49ed1ca46ad57b7d0078c277f45c
pann5 <pann5@mail2.sysu.edu.cn>
2018-08-31 14:26:02 +0800

    assets small2

---
+++ Assets/Panel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+
+    public Text BrandText;
+    public Text ProductNameText;
+    public Text GradeText;
+    public Text ColorText;
+    public Text CompositionText;
+    public Text PriceText;
+
+    public void ShowContent(string brand, string productname, string grade, string color, string composition, string price)
+    {
+        BrandText.text = brand;
+        ProductNameText.text = productname;
+        GradeText.text = grade;
+        ColorText.text = color;
+        CompositionText.text = composition;
+        PriceText.text = price;
+
+    }
+
+}
---
+++ Assets/getcomponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getcomponent : MonoBehaviour {
+
+    public Transform[] GetTransforms;
+
+    void start ()
+    {
+        GetTransforms = GetComponentsInChildren<Transform>(true);
+
+        foreach (Transform child in GetTransforms)
+        {
+            print(child.name);
+        }
+    }
+    void update ()
+    {
+
+    }
+        
+
+}
