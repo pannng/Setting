0b64e0a6bd00bba6c70cc40e9368ed717cd8f4aa
pann5 <pann5@mail2.sysu.edu.cn>
2018-08-31 13:50:54 +0800

    VRTK

---
+++ Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/PanelMenu/PanelMenuDemoFlyingSaucer.cs
@@ -0,0 +1,31 @@
+﻿namespace VRTK.Examples.PanelMenu
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Demo component for Panel Menu example scene.
+    /// </summary>
+    /// <example>
+    /// See the demo scene for a complete example: [ 040_Controls_Panel_Menu ]
+    /// </example>
+    public class PanelMenuDemoFlyingSaucer : MonoBehaviour
+    {
+
+        private readonly Color[] colors =
+        {
+        Color.black,
+        Color.blue,
+        Color.cyan,
+        Color.gray,
+        Color.green,
+        Color.magenta,
+        Color.red,
+        Color.white
+    };
+
+        public void UpdateGridLayoutValue(int selectedIndex)
+        {
+            transform.GetComponent<MeshRenderer>().materials[1].color = colors[selectedIndex];
+        }
+    }
+}
