a4288325a17e356af81dc1f9813acc66f4d8e645
pann5 <pann5@mail2.sysu.edu.cn>
2018-09-06 16:56:46 +0800

    audio time

--- Assets/RotationFeedback.cs
+++ Assets/RotationFeedback.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class RotationFeedback : MonoBehaviour {
 
     public float presentRotationY;
-    
+
+    public float CoolDownTime = 0.2F;
+    float CoolDownLeft = 0.0F;
+
     public AudioClip clip;
 
     void Start()
@@ -16,7 +19,18 @@ public class RotationFeedback : MonoBehaviour {
     void Update()
     {
         presentRotationY = transform.eulerAngles.y;
-        if (presentRotationY > 179 && presentRotationY < 181)
+
+        if (CoolDownLeft > 0.0f)
+        {
+            CoolDownLeft -= Time.deltaTime;
+            if (CoolDownLeft < 0.0F)
+            {
+                CoolDownLeft = 0.0F;
+            }
+
+        }
+
+        if (presentRotationY > 179 && presentRotationY < 181 && CoolDownLeft <= 0.0F)
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
         }
--- Assets/ScalingFeedback.cs
+++ Assets/ScalingFeedback.cs
@@ -6,6 +6,9 @@ public class ScalingFeedback : MonoBehaviour {
 
     public float presentScaleX;
 
+    public float CoolDownTime = 0.2F;
+    float CoolDownLeft = 0.0F;
+
     public AudioClip clip;
 
     void Start()
@@ -15,7 +18,18 @@ public class ScalingFeedback : MonoBehaviour {
 
     void Update () {
         presentScaleX = transform.localScale.x;
-        if (presentScaleX > 1.99 && presentScaleX < 2.01)
+
+        if (CoolDownLeft > 0.0f)
+        {
+            CoolDownLeft -= Time.deltaTime;
+            if (CoolDownLeft < 0.0F)
+            {
+                CoolDownLeft = 0.0F;
+            }
+
+        }
+
+        if (presentScaleX > 1.99 && presentScaleX < 2.01 && CoolDownLeft <= 0.0F)
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
         }
