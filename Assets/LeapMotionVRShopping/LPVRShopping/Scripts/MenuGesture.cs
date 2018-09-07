48553a2bd6d91a94090ad1e00545a09b98fdcea2
pann5 <pann5@mail2.sysu.edu.cn>
2018-09-07 14:19:37 +0800

    MenuGesture

--- Assets/LeapMotionVRShopping/LPVRShopping/Scripts/MenuGesture.cs
+++ Assets/LeapMotionVRShopping/LPVRShopping/Scripts/MenuGesture.cs
@@ -7,27 +7,44 @@ public class MenuGesture : GestureBase
     public EHand m_Hand;
     public EHandAxis m_HandAxis;
     public EDirection m_Direction;
-    public float m_TimeRange = 1f;
+    bool alreadyFist = false;
+
+    //Sequence Gesture
+    public float m_TimeRange = 2f;
+
+    FingerExtendedDetails m_GestureDetail;
+    [Range(0.0f, 1.0f)]
+    public float m_ClosedPercentage = 0.6f;
 
     float m_CoolDownLeft = 0.0f;
     bool m_IsFist = false;
+    bool m_IsHandAllExtended = false;
 
-    [Range(0.0f, 1.0f)]
-    public float m_ClosedPercentage = 0.6f;
+    //public MenuManager m_menumanager;
+
+
+
 
     Dictionary<EDirection, Vector3> m_DirectionMap = new Dictionary<EDirection, Vector3>();
 
     void Start ()
-    {
+    {   //For hand direction
         m_DirectionMap.Add(EDirection.eUpwards, Vector3.up);
         m_DirectionMap.Add(EDirection.eDownwards, Vector3.down);
         m_DirectionMap.Add(EDirection.eLeft, Vector3.left);
         m_DirectionMap.Add(EDirection.eRight, Vector3.right);
         m_DirectionMap.Add(EDirection.eInWards, Vector3.back);
         m_DirectionMap.Add(EDirection.eOutwards, Vector3.forward);
+
+        //For hand extended
+        m_GestureDetail.bThumbExtended = true;
+        m_GestureDetail.bIndexExtended = true;
+        m_GestureDetail.bMiddleExtended = true;
+        m_GestureDetail.bRingExtended = true;
+        m_GestureDetail.bPinkeyExtended = true;
     }
 	
-
+    //Set a Timer
 	void Update () 
     {
         if (m_CoolDownLeft > 0.0f)
@@ -36,11 +53,15 @@ public class MenuGesture : GestureBase
             if (m_CoolDownLeft < 0.0f)
             {
                 m_CoolDownLeft = 0.0f;
+                alreadyFist = false;
+                m_IsHandAllExtended = false;
+
             }
         }
 
     }
 
+    //Get the current hand direction
     EDirection GetClosestDirection(ref bool a_bDetected)
     {
         DetectionManager.DetectionHand detectionHand = DetectionManager.Get().GetHand(m_Hand);
@@ -71,6 +92,8 @@ public class MenuGesture : GestureBase
         return currentDir;
     }
 
+    //Phase 1 -- Fist hand
+    //Decide whether is Fist or not
     void IsFist()
     {
         if (DetectionManager.Get().IsHandSet(m_Hand))
@@ -79,36 +102,60 @@ public class MenuGesture : GestureBase
             {
                 m_CoolDownLeft = m_TimeRange;
                 m_IsFist = true;
-                Debug.Log("Detect the first phase of MenuGesture-Fist");
+                alreadyFist = true;
+                //Debug.Log("Detect the first phase of MenuGesture-Fist");
+                m_IsHandAllExtended = false;
+                return;
             }
             //return DetectionManager.Get().GetHand(m_Hand).IsClosed(m_ClosedPercentage);
         }
 
-        m_IsFist = false;
     }
 
+    void IsHandAllExtended()
+    {
+        DetectionManager.DetectionHand detectHand = DetectionManager.Get().GetHand(m_Hand);
 
+        if (detectHand.IsSet())
+        {
+            if(detectHand.CheckWithDetails(m_GestureDetail))
+            {
+                m_IsHandAllExtended = true;
+                Debug.Log("success");
+                //m_menumanager.PresentingthisObjectMenu();
+                alreadyFist = false;
+                //reset
+                return;
+            }
+        }
+        //m_IsHandAllExtended = false;
+    }
+
+    //Phase 2 -- Hand open upwards
+    //if hand has fisted 
+    //if it is still in the time range 
+    //if the hand direction matches
     public override bool Detected()
     {
         bool bFound = false;
         EDirection currentDirection = GetClosestDirection(ref bFound);
 
-        IsFist();
-
-        if (bFound && m_IsFist && m_CoolDownLeft > 0.0f)
-        {
-            if(currentDirection == m_Direction)
+        if (!alreadyFist){
+            IsFist();
+        }else{
+            if (bFound && m_CoolDownLeft > 0.0f)
             {
-                m_IsFist = false;
-                Debug.Log("Detect the second phase of MenuGesture-HandOn");
-                return true;
+                if (currentDirection == m_Direction)
+                {
+
+                    IsHandAllExtended();
+                }
+
             }
-
-            m_IsFist = false;
-
         }
-        m_IsFist = false;
-        return false;
+
+        return m_IsHandAllExtended;
+        //IsFist();
     }
 
 
