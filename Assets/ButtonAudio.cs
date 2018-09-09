0ea04046d4399bf7c6360faa19be2ae9df1c042d
lulumbp15 <luoweizhou23@163.com>
2018-09-09 13:23:16 +0800

    LPButtonAudio

---
+++ Assets/ButtonAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour {
+
+    public AudioClip m_buttonAudio;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    public void PlayButtonAudio()
+    {
+        AudioSource.PlayClipAtPoint(m_buttonAudio, transform.position);
+    }
+}
